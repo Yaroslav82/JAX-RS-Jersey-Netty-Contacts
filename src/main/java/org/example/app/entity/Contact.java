0da43265ed3b37a819f76aacbff63691d7667908
Yaroslav82 <yargay230607@gmail.com>
2023-10-04 12:55:50 +0300

    Create entity contact

---
+++ src/main/java/org/example/app/entity/Contact.java
@@ -0,0 +1,15 @@
+package org.example.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+
+    private Long id;
+    private String name;
+    private String email;
+}
