0f486ef0b11a287f4affa0908bbf2ad3d92030c7
Yaroslav82 <yargay230607@gmail.com>
2023-10-04 13:35:11 +0300

    Create main class

---
+++ src/main/java/org/example/app/App.java
@@ -0,0 +1,12 @@
+package org.example.app;
+
+import org.example.app.utils.AppStarter;
+
+import java.net.MalformedURLException;
+
+public class App {
+
+    public static void main(String[] args) throws MalformedURLException {
+        AppStarter.runApp();
+    }
+}
